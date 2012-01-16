2670b2a4d59a464a6a3d7b9b34448beaadc69337
Ian Hinder <ian.hinder@aei.mpg.de>
2012-01-16 14:59:50 +0100

    EccentricPNSymbols.m: Add l to list of reserved symbols

--- EccentricPNSymbols.m
+++ EccentricPNSymbols.m
@@ -1,7 +1,7 @@
 
 BeginPackage["EccentricPNSymbols`"];
 
-{n, e, u, x, eta, R, r, phiDot, th, ph, rDot, phi, Eps, j, eph};
+{n, e, u, x, l, eta, R, r, phiDot, th, ph, rDot, phi, Eps, j, eph};
 
 Begin["`Private`"];
 
